15aa7200456546c5deec6f52ff5b450496f22d92
NoboNobo <irieda@gmail.com>
2026-02-07 14:43:08 +0900

    wip

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { CircleUserIcon, MenuIcon } from "@lucide/svelte";
+  import { AppBar } from "@skeletonlabs/skeleton-svelte";
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+<AppBar>
+  <AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
+    <AppBar.Lead>
+      <button type="button" class="btn-icon btn-icon-lg hover:preset-tonal"
+        ><MenuIcon /></button
+      >
+    </AppBar.Lead>
+    <AppBar.Headline>
+      <p class="text-2xl">Gun Shooter</p>
+    </AppBar.Headline>
+    <AppBar.Trail>
+      <button type="button" class="btn-icon hover:preset-tonal"
+        ><CircleUserIcon class="size-6" /></button
+      >
+    </AppBar.Trail>
+  </AppBar.Toolbar>
+</AppBar>
+<div class="page-container">
+  {@render children()}
+</div>
